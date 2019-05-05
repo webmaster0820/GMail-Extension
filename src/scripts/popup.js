a840031aca7de8a34d9ca2bf2a852b028c274244
Sebastian Corradini <sebastian34237@gmail.com>
2019-05-05 11:34:54 -0300

    Added option pages

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,87 +1,11 @@
 import isUndefined from 'lodash.isundefined';
-import platform from 'utils/platform';
-import { onPopupOpen, onPopupClose, opengoldtimeWebsite, popupClose } from 'utils/actions';
-import { send, listen } from 'utils/messaging';
 import Vue from 'vue';
 import App from './popup/App.vue';
 
-const DATA = { serverCheckin: null, navigated: null, user: null, companies: [], balances: [], videos: [], loading: true, errors: {} };
+const DATA = { };
 
 new Vue({
   el: '#popup',
   data: DATA,
   components: { App },
-  methods: {
-    loginRequest(event) {
-      const { target: form } = event;
-      send('popup.request.login', { login: form.login.value, password: form.password.value });
-    },
-    logoutRequest() {
-      send('popup.request.logout');
-    },
-    openWebsite() {
-      opengoldtimeWebsite(() => popupClose());
-    },
-    navigate(screen) {
-      this.navigated = screen;
-    },
-    isScreenActive(screen) {
-      if (this.navigated) {
-        return screen == this.navigated;        
-      }
-      switch (screen) {
-        case 'profile': return this.videos.length == 0;
-        case 'videos': return this.videos.length > 0;
-      }
-    },
-    setActiveCompanyId(companyId) {
-      send('popup.request.switch_company', { company_id: companyId })
-      this.navigate('profile');
-    },
-  }
 });
-
-onPopupOpen(() => {
-  DATA.loading = true;
-  DATA.navigated = null;
-  send('popup.focus');
-});
-
-onPopupClose(() => {
-  // popupClose();
-  send('popup.blur');
-});
-
-listen('popup.state', (state) => {
-  DATA.loading = !! state.loading;
-  DATA.serverCheckin = !isUndefined(state.serverCheckin) ? state.serverCheckin : DATA.serverCheckin;
-  DATA.user = !isUndefined(state.user) ? state.user : DATA.user;
-  DATA.companies = !isUndefined(state.companies) ? state.companies : DATA.companies;
-  DATA.balances = !isUndefined(state.balances) ? state.balances : DATA.balances;
-  DATA.videos = !isUndefined(state.videos) ? state.videos : DATA.videos;
-  DATA.errors = !isUndefined(state.errors) ? state.errors : {};
-});
-
-if (platform === 'safari_deprecated') {
-  safari.self.width = 300;
-  const layout = document.getElementById('popup')
-  setInterval(() => {
-    const desiredHeight = document.body.clientHeight + 20;
-    if (desiredHeight !== safari.self.height) {
-      safari.self.height = desiredHeight;
-    }
-  }, 300);
-}
-
-if (platform == 'safari') {
-  send('popup.focus');
-}
-
-/**
- * Connect LiveReload for development purposes
- */
-if (platform == 'chrome' && __goldtime_EXTENSION_CONFIG__.env === 'development') {
-  const script = document.createElement('script');
-  script.src = 'scripts/livereload.js';
-  document.head.appendChild(script);
-}
--- src/scripts/popup/App.vue
+++ src/scripts/popup/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="layout">
+    <a href="#" @click="openOption()">Options</a>
   </div>
 </template>
 
 <script>
-import Loading from './components/Loading.vue';
 
 export default {
-  data() {
-    return { navigated: null };
-  },
-  components: {
-    Loading,
+  methods:{
+    openOption(){
+      window.open("options.html");
+    }
   }
 }
 </script>
@@ -22,86 +21,5 @@ export default {
     font-size: 14px;
     display: flex;
     flex-direction: column;
-  }
-  .footer {
-    margin-top: auto;
-  }
-  .prevent_using {
-    pointer-events: none;
-    opacity: .3;
-  }
-  .current-user {
-    margin-bottom: 10px;
-  }
-  .log-out-action:hover {
-    text-decoration: underline;
-  }
-  .loading .lds-spinner {
-    zoom: 0.7;
-  }
-  .logo {
-    opacity: .4;
-    display: block;
-    text-align: center;
-    margin-top: 15px;
-    margin-bottom: 15px;
-    color: #7d90fd;
-    text-decoration: none;
-    font-size: 16px;
-  }
-  .logo:hover {
-    opacity: .6;
-  }
-  .logo img {
-    width: 110px;
-    vertical-align: sub;
-  }
-  .top-slot a, .bottom-slot a {
-    color: #717070;
-    text-decoration: none;
-    display: block;
-    text-align: center;
-    margin-top: 10px;
-  }
-  .top-slot a:hover, .bottom-slot a:hover {
-    text-decoration: underline;
-  }
-</style>
-
-<style>
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url('fonts/proximanova-reg-webfont.woff2') format('woff2'),
-    url('fonts/proximanova-reg-webfont.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Proxima Nova';
-    src: url('fonts/proximanova-semibold-webfont.woff2') format('woff2'),
-    url('fonts/proximanova-semibold-webfont.woff') format('woff');
-    font-weight: bold;
-    font-style: normal;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  html, body {
-    font-family: 'Proxima Nova';
-    background-color: #ffffff;
-    margin: 0;
-    padding: 0;
-    height: 100%;
-  }
-  body {
-    width: 300px;
-    min-height: 400px;
-    padding-top: 20px;
-    padding-left: 20px;
-    padding-right: 20px;
-    padding-bottom: 10px;
-  }
-  #popup {
-    height: 100%;
   }
 </style>
